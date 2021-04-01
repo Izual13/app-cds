f519a6d8875f2bd3e40e66882bf4b18c90baee17
Maxim Tsoy <mtsoy1990@gmail.com>
2021-04-01 23:24:58 +0300

    jetty + spring

--- src/main/java/com/template/hello/HelloApplication.java
+++ src/main/java/com/template/hello/HelloApplication.java
@@ -1,55 +1,71 @@
 package com.template.hello;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.http.MediaType;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
+import java.io.IOException;
+
 public class HelloApplication {
 
-
-    @Bean
-    public WebClient webClient() {
-        return WebClient.create();
-    }
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         long start = System.nanoTime();
         System.out.println("http://localhost:8080");
-        new SpringApplicationBuilder(HelloApplication.class)
-                .logStartupInfo(false)
-                .run(args);
+        Server server = new Server(8080);
 
+        server.setHandler(getServletContextHandler());
+
+        server.start();
+        server.join();
         System.out.println(String.format("startup: %s ms", (System.nanoTime() - start) / 1_000_000));
     }
 
 
+    private static ServletContextHandler getServletContextHandler() throws IOException {
+        ServletContextHandler contextHandler = new ServletContextHandler(ServletContextHandler.NO_SECURITY);
+        contextHandler.setErrorHandler(null);
+
+        // Spring
+        WebApplicationContext webAppContext = getWebApplicationContext();
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(webAppContext);
+        ServletHolder springServletHolder = new ServletHolder("mvc-dispatcher", dispatcherServlet);
+        contextHandler.addServlet(springServletHolder, "/");
+
+        return contextHandler;
+    }
+
+    private static WebApplicationContext getWebApplicationContext() {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.setConfigLocation("com.template.hello");
+        return context;
+    }
+
     @RestController
-    @RequestMapping("/api")
-    @RequiredArgsConstructor
+    @RequestMapping("/")
     public static class HelloController {
 
-        private final WebClient webClient;
-
-        @GetMapping("/hello")
-        public Mono<String> hello() {
-            return Mono.create(s -> s.success("Ok!"));
+        @GetMapping
+        public String hello() {
+            return "Ok!";
         }
 
-        @GetMapping(value = "/cat", produces = MediaType.IMAGE_PNG_VALUE)
-        private Mono<byte[]> getCat() {
-            return webClient.get().uri("https://cataas.com/cat").exchange()
-                    .flatMap(response -> response.bodyToMono(ByteArrayResource.class))
-                    .map(ByteArrayResource::getByteArray);
-        }
+    }
+
+    @Configuration
+    @EnableWebMvc
+    @ComponentScan(basePackages = "com.template.hello")
+    public class WebMvcConfig implements WebMvcConfigurer {
+
     }
 
 }
